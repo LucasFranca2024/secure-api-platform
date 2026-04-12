86e3ddebee8d5a75e9d3d9232f410c8b612039a9
LucasFranca2024 <157262537+LucasFranca2024@users.noreply.github.com>
2026-04-12 20:37:43 -0300

    feat: initialize FastAPI app with database integration
    - initialize FastAPI application
    - connect SQLAlchemy engine to database
    - register User model for table creation
    - enable automatic database table creation on startup
    - define base root endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from app.database.database import Base, engine
+from app.models import user
 
 app = FastAPI()
 
+Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 def home():
-    return {"message": "Secure API is running 🚀"}
+    return {"message": "Secure API is running"}
